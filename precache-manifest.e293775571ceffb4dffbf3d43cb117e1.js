f65634b15f723325e9d0436d43d8a31862207e2c
Oventura19 <47031203+Oventura19@users.noreply.github.com>
2020-06-16 17:11:45 -0500

    Updates

---
+++ precache-manifest.e293775571ceffb4dffbf3d43cb117e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b964e964c1625633a790e7487e7d4d9e",
+    "url": "/cronometros/index.html"
+  },
+  {
+    "revision": "41325ce3d5e9ed169498",
+    "url": "/cronometros/static/css/main.8d4b81e6.chunk.css"
+  },
+  {
+    "revision": "ef9a73619eac38d2b337",
+    "url": "/cronometros/static/js/2.e53482c3.chunk.js"
+  },
+  {
+    "revision": "4a25f85bcfe852f99d2a05ccc6fcb887",
+    "url": "/cronometros/static/js/2.e53482c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41325ce3d5e9ed169498",
+    "url": "/cronometros/static/js/main.a1a93c01.chunk.js"
+  },
+  {
+    "revision": "bcf1f494e3d7c33cf8fd",
+    "url": "/cronometros/static/js/runtime-main.68fb5245.js"
+  }
+]);
